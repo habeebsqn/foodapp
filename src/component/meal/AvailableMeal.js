06a676a0ef40d9df131d8de2c8c9d679a21ef06d
habeeb <t.sholanke5@gmail.com>
2023-02-01 23:11:33 +0100

    implemented function to fetch the available meal api

--- src/component/meal/AvailableMeal.js
+++ src/component/meal/AvailableMeal.js
@@ -1,23 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import classes from "./AvailableMeal.module.css";
 import MealItem from "./MealItem";
 import Card from "../UI/Card";
-import DUMMY_MEALS from "./DummyMeals";
 
 const AvailableMeal = () => {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const fetchmealsHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-be4bf-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      const loaded = [];
+
+      for (const key in data) {
+        loaded.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+
+      setLoadedMeals(loaded);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchmealsHandler();
+  }, [fetchmealsHandler]);
+
+  const listOfMealS = loadedMeals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    ></MealItem>
+  ));
+
   return (
     <div className={classes.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((meal) => (
-            <MealItem
-              id={meal.id}
-              key={meal.id}
-              name={meal.name}
-              description={meal.description}
-              price={meal.price}
-            ></MealItem>
-          ))}
+          {isLoading && !error ? <p>THE MEALS ARE COMING!</p> : listOfMealS}
+          {!isLoading && error && <p>{error}</p>}
         </ul>
       </Card>
     </div>
